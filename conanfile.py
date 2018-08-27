192dc42d850fd3cce3b5bc6f75327f0f8f3984e3
CD <cdickson@aquaveo.com>
2018-08-27 13:50:59 -0600

    Fix Doxyfile, add commented conan xmsextractor.

--- conanfile.py
+++ conanfile.py
@@ -46,6 +46,10 @@ class XmsgridtraceConan(ConanFile):
         self.options['xmsgrid'].pybind = self.options.pybind
         self.options['xmsgrid'].testing = self.options.testing
 
+        #self.options['xmsextractor'].xms = self.options.xms
+        #self.options['xmsextractor'].pybind = self.options.pybind
+        #self.options['xmsextractor'].testing = self.options.testing
+
         if s_compiler == "apple-clang" and s_os == 'Linux':
             raise ConanException("Clang on Linux is not supported.")
 
@@ -67,11 +71,11 @@ class XmsgridtraceConan(ConanFile):
                 and self.options.pybind:
             self.requires("pybind11/2.2.2@aquaveo/stable")
 
-        # Use the dev version of XMSCore, XMSInterp, and XMSGrid
+        # Use the dev version of XMSCore, XMSInterp, XMSGrid, XMSExtractor
         self.requires("xmscore/[>=1.0.37]@aquaveo/stable")
         self.requires("xmsinterp/[>=1.0.16]@aquaveo/stable")
         self.requires("xmsgrid/[>=1.0.8]@aquaveo/stable")
-        # self.requires("xmsextractor/[>=1.0.0]@aquaveo/stable")
+        # self.requires("xmsextractor/[>=0.0.1]@aquaveo/stable")
 
     def build(self):
         cmake = CMake(self)
