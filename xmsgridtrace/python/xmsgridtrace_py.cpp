d5654f419a8267110e0f2293923708a5908e55f2
glarsen <glarsen@aquaveo.com>
2018-12-04 12:22:18 -0700

    add python version

--- xmsgridtrace/python/xmsgridtrace_py.cpp
+++ xmsgridtrace/python/xmsgridtrace_py.cpp
@@ -13,16 +13,15 @@
 namespace py = pybind11;
 
 //----- Python Interface -------------------------------------------------------
-std::string version() {
-    return "1.0.0";
-}
+#ifndef XMS_VERSION
+  #define XMS_VERSION "99.99.99";
+#endif
 
 
 //------ Primary Module --------------------------------------------------------
 PYBIND11_MODULE(xmsgridtrace, m) {
     m.doc() = "Python bindings for xmsgridtrace"; // optional module docstring
-    m.def("version", &version,
-          "Get current version of xmsgridtrace Python bindings.");
+    m.attr("__version__") = XMS_VERSION;
 
     const char* misc_doc = R"pydoc(
         The gridtrace module of the xmsgridtrace python library contains classes
